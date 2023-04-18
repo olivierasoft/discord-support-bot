9605c30f6837c635e1388f7509c00df35b2a2fa9
Renan Oliveira <renan.oliveira@pisomtech.com.br>
2023-04-18 01:04:29 -0300

    Removido algumas partes desnecessárias no codigo, ajustado path alises

--- src/modules/ticket/usecases/handle-interaction.usecase.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { Client } from 'discord.js';
-import { ConfigureSupportChannelUseCase } from './configure-support-channel.usecase';
-
-@Injectable()
-export class HandleInteractionUseCase {
-  constructor(private client: Client) {}
-}
